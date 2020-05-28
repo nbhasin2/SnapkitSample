8ecbb890da5fcd63d77c8af7a917c872324f0e5c
Nishant Bhasin <nikieme3@gmail.com>
2020-05-28 14:29:26 -0400

    Initial Commit

---
+++ SnapKitSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SnapKitSample
+//
+//  Created by Nishant Bhasin on 2020-05-28.
+//  Copyright © 2020 Nishant Bhasin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
